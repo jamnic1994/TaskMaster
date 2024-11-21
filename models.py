e1e0fab3a49523ed0dfc5a81934d5ac30f3806c0
jamnic1994 <jamie.nic1@gmail.com>
2024-11-21 04:50:51 +0000

    Added completed task and account details functionality

--- models.py
+++ models.py
@@ -1,6 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
-from werkzeug.security import check_password_hash, generate_password_hash
 
 db = SQLAlchemy()
 
@@ -12,8 +10,8 @@ class TaskGroup(db.Model):
     description = db.Column(db.String(255))           # Optional description
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)  # Owner of the group
 
-    # Relationship to tasks
-    tasks = db.relationship('Task', backref='group', lazy=True)
+    # Relationship to User (Cascade delete tasks when user is deleted)
+    user = db.relationship('User', backref=db.backref('taskGroup', cascade='all, delete'), lazy=True)
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -24,9 +22,6 @@ class User(db.Model):
     first_name = db.Column(db.String(150), nullable=False)
     surname = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
-
-    # Define the relationship with Task
-    tasks = db.relationship('Task', backref='user', lazy=True)
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -60,7 +55,22 @@ class Task(db.Model):
     description = db.Column(db.Text)
     status = db.Column(db.String(50), default='Pending')
     group_id = db.Column(db.Integer, db.ForeignKey('taskGroup.id'), nullable=True)  # Foreign key to group
-
-    # Define the foreign key linking to User
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
+    # Relationship to User (Cascade delete tasks when user is deleted)
+    user = db.relationship('User', backref=db.backref('tasks', cascade='all, delete'), lazy=True)
+
+class CompletedTask(db.Model):
+    __tablename__ = 'completedTasks'
+
+    completed_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) # Foreign key to users
+    title = db.Column(db.String(100), nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('taskGroup.id'), nullable=True)  # Foreign key to TaskGroup
+    completed_date = db.Column(db.Date, nullable=False)
+
+    # Relationship to User (Cascade delete completed tasks when user is deleted)
+    user = db.relationship('User', backref=db.backref('completed_tasks', cascade='all, delete'), lazy=True)
+
+    def __repr__(self):
+        return f"<CompletedTask {self.completed_id} - {self.title}>"
